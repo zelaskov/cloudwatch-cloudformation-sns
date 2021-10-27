a861eabdaff5185f688529d54157713bf310915e
zelaskov <marcin.zelasko@codetain.eu>
2021-10-27 13:20:02 +0200

    infra: add terraform template for sns topic

---
+++ sns.tf
@@ -0,0 +1,28 @@
+resource "aws_sns_topic" "sns-topic" {
+  name = "cloudformation-stack-delete"
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = aws_sns_topic.sns-topic.arn
+  policy = data.aws_iam_policy_document.sns-topic-policy.json
+}
+
+resource "aws_sns_topic_subscription" "sns-mail-subscription" {
+  topic_arn = aws_sns_topic.sns-topic.arn
+  protocol  = "email"
+  endpoint  = "marcin.zelasko@codetain.com"
+}
+
+data "aws_iam_policy_document" "sns-topic-policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [aws_sns_topic.sns-topic.arn]
+  }
+}
